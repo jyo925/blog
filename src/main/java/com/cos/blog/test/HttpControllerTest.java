c6e56e6f6a6b1a63468815b21a024b6096f654c2
지윤 <45502553+jyo925@users.noreply.github.com>
2021-05-04 15:31:09 +0900

    5/4 테이블 생성중

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -8,14 +8,14 @@ public class HttpControllerTest {
     private static final String TAG = "HttpControllerTest : ";
 
     // localhost:8000/blog/http/lombok
-/*    @GetMapping("/http/lombok")
+    @GetMapping("/http/lombok")
     public String lombokTest() {
         Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
         System.out.println(TAG+"getter : "+m.getUsername());
         m.setUsername("cos");
         System.out.println(TAG+"setter : "+m.getUsername());
         return "lombok test 완료";
-    }*/
+    }
 
 
     // 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
